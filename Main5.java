2f1c3fd4d6ade030be98b0a38e9380db0e5c1cdb
Kaviya <732125104051.25.cse@nandhatech.org>
2026-07-04 08:32:04 +0530

    Create Main5.java

---
+++ Main5.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Rahul";
+        int followers = 25000;
+        String platform = "YouTube";
+
+        System.out.println("Name : " + name);
+        System.out.println("Followers : " + followers);
+        System.out.print("Platform : ");
+        System.out.println(platform);
+    }
+}
